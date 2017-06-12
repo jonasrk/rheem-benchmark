acdcf234f0b2165ef7f3aaaf3646fc09079d7ef7
Jonas <jonas.kemper.biz@gmail.com>
2017-06-12 18:26:52 +0200

    next step of v1 cleaner select repo

--- src/main/scala/org/qcri/rheem/apps/wordcount/WordCountScala.scala
+++ src/main/scala/org/qcri/rheem/apps/wordcount/WordCountScala.scala
@@ -35,15 +35,16 @@ class WordCountScala(plugin: Plugin*) {
       .withExperiment(experiment)
       .withUdfJarsOf(this.getClass)
       .readTextFile(inputUrl).withName("Load file")
-      .flatMap(_.split("\\W+")//,
-//        udfLoad = LoadProfileEstimators.createFromSpecification(
-//          "my.udf.costfunction.key", configuration
-//        ),
-//        udfSelectivity = ProbabilisticDoubleInterval.createFromSpecification(
-//          "my.udf.wordcount.flatmap", configuration
-//        )
+      .flatMap(_.split("\\W+"),
+        udfSelectivity = ProbabilisticDoubleInterval.createFromSpecification(
+          "my.udf.wordcount.flatmap", configuration
+        )
        ).withName("Split words")
-      .filter(_.nonEmpty, selectivity = 0.99).withName("Filter empty words")
+      .filter(_.nonEmpty,
+        udfSelectivity = ProbabilisticDoubleInterval.createFromSpecification(
+          "my.udf.wordcount.filter", configuration
+        )
+      ).withName("Filter empty words")
       .map(word => (word.toLowerCase, 1)).withName("To lower case, add counter")
       .reduceByKey(_._1, (c1, c2) => (c1._1, c1._2 + c2._2)).withName("Add counters")
       .withCardinalityEstimator((in: Long) => math.round(in * 0.01))
